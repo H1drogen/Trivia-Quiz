a385080edf87a2ccd9460e8cead428eda175ac9c
SimonH <professional.simonhossain@gmail.com>
2024-03-18 01:07:28 +0000

    Initialising maven project

---
+++ src/test/java/projects/triviaquiz/TriviaQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.triviaquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
